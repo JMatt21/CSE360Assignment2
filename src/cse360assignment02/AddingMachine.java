5aaad4740abfc6b900367949b486971362115a65
Matthew Bertrandt <matthewbertrandt@gmail.com>
2020-10-03 14:06:23 -0700

    Second version with implemented methods

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,43 +2,50 @@ package cse360assignment02;
 
 public class AddingMachine {
 	  private int total;
+	  private String history;
 	  
 	  /**
 	   * Constructor for our class.
-	   * Initializes total to be 0.
+	   * Initializes total to be 0 and history to be "0".
 	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  /**
-	   * @return total
+	   * @return total int
 	   */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  /**
-	   * Takes value and adds it to total.
+	   * Takes value and adds it to total. Append " + " + value to history.
 	   * @param value int
 	   */
 	  public void add (int value) {
-
+		  total += value;
+		  history += " + " + value;
 	  }
 	  /**
-	   * Takes value and subtracts it from total.
+	   * Takes value and subtracts it from total. Append " - " + value to history.
 	   * @param value int
 	   */
 	  public void subtract (int value) {
-
+		  total -= value;
+		  history += " - " + value;
 	  }
-
+	  /**
+	   * @return history
+	   */
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
 
 	  /**
-	   * Resets object to a blank slate by setting total to 0 and history to "".
+	   * Resets AddingMachine object to a blank slate by setting total to 0 and history to "0".
 	   */
 	  public void clear() {
-
+		  total = 0;
+		  history = "0";
 	  }
 	}
--- src/cse360assignment02/Tester.java
+++ src/cse360assignment02/Tester.java
@@ -1,9 +1,15 @@
 package cse360assignment02;
-
+import cse360assignment02.AddingMachine;
 public class Tester {
 
 	public static void main(String[] args) {
 		
+		AddingMachine myAddingMachine = new AddingMachine();
+		System.out.println(myAddingMachine.toString());
+		myAddingMachine.add(4);
+		myAddingMachine.subtract(2);
+		myAddingMachine.add(5);
+		System.out.println(myAddingMachine.toString());
 	}
 
 }
