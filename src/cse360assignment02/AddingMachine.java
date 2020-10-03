ded0cd70ef943bf8b27e8c5b701876b91067431a
Matthew Bertrandt <matthewbertrandt@gmail.com>
2020-10-03 13:55:04 -0700

    initial version

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	  private int total;
+	  
+	  /**
+	   * Constructor for our class.
+	   * Initializes total to be 0.
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  /**
+	   * @return total
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  /**
+	   * Takes value and adds it to total.
+	   * @param value int
+	   */
+	  public void add (int value) {
+
+	  }
+	  /**
+	   * Takes value and subtracts it from total.
+	   * @param value int
+	   */
+	  public void subtract (int value) {
+
+	  }
+
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * Resets object to a blank slate by setting total to 0 and history to "".
+	   */
+	  public void clear() {
+
+	  }
+	}
---
+++ src/cse360assignment02/Tester.java
@@ -0,0 +1,9 @@
+package cse360assignment02;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
